9208790ef4cbf49b6f0f26013cf43e2ac5b67d20
Goudar <113749395+karthikgoudar@users.noreply.github.com>
2023-02-02 18:02:04 +0530

    Update QuickSort.py

--- Sorting Algorithms/QuickSort.py
+++ Sorting Algorithms/QuickSort.py
@@ -5,8 +5,9 @@ Date   : 1 Feb 2023
 
 Simple Quick Sort Algorithm
 
-Quick sort is a divide-and-conquer algorithm that works by selecting a "pivot" element from the array and partitioning the other elements into two sub-arrays,
- according to whether they are less than or greater than the pivot. The sub-arrays are then sorted recursively.
+Quick sort is a divide-and-conquer algorithm that works by selecting a "pivot" element
+from the array and partitioning the other elements into two sub-arrays,
+according to whether they are less than or greater than the pivot. The sub-arrays are then sorted recursively.
 
 
 the pivot element is selected and used to partition the array into two sub-arrays.
