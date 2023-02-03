a60f316a3da2ab2252837b32ac2984d7700bb40c
Goudar <113749395+karthikgoudar@users.noreply.github.com>
2023-02-03 22:14:57 +0530

    Add files via upload

---
+++ Searching Algorithms/BinarySearch.py
@@ -0,0 +1,31 @@
+'''
+
+Binary Search
+
+Author : Karthik Goudar
+Date   : 3 Feb, 2023
+'''
+
+
+
+def binary_search(arr, target):
+    left = 0
+    right = len(arr) - 1
+    while left <= right:
+        mid = (left + right) // 2
+        if arr[mid] == target:
+            return mid
+        elif arr[mid] < target:
+            left = mid + 1
+        else:
+            right = mid - 1
+    return -1
+
+# Example usage
+arr = [1, 3, 4, 5, 6, 7, 8, 9]
+target = 7
+result = binary_search(arr, target)
+if result != -1:
+    print(f"Element is present at index {result}")
+else:
+    print("Element is not present in array")
