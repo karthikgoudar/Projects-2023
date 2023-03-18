42fd9147d9a5e0895ff3905cc20d7a4902e9bd6a
Goudar <113749395+karthikgoudar@users.noreply.github.com>
2023-03-18 21:48:06 +0530

    Add files via upload

---
+++ Design_a_School_Project/course.py
@@ -0,0 +1,45 @@
+from professor import Professor
+from enroll import Enroll
+
+class Course:
+    def __init__(self, name, code, max_, min_, professor):
+        self.name = name
+        self.code = code
+        self.max = max_
+        self.min = min_
+        self.professors = []
+        self.enrollments = []
+
+
+        if isinstance(professor, Professor) :
+            self.addresses.append(professor)
+        elif isinstance(professor, list):
+            for entry in professor:
+                if not isinstance(entry, Professor):
+                    raise Error("Invalid Address...")
+                
+            self.professors = professor
+
+        else:
+            raise Error("Invalid Address...")
+        
+
+    def add_professor(self, professor):
+        if not isinstance(professor, Professor):
+            raise Error("Invalid professor...")
+        
+        self.professors.append(professor)
+
+
+    def add_enrollment(self, enroll):
+        if not isinstance(enroll, Enroll):
+            raise Error("Invalid Enroll...")
+        
+
+        if len(self.enrollments) == self.max:
+            raise Error("Cannot enroll, course is full...")
+
+        self.enrollments.append(enroll)
+
+    def is_cancelled(self):
+        return len(self.enrollments) < self.min 
